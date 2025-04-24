0099419681437e2cde6dc55655280a937e1e085e
Xample33 <54323615+Xample33@users.noreply.github.com>
2025-04-24 15:34:22 +0200

    skip failed test

--- test/calculator.fail.test.js
+++ test/calculator.fail.test.js
@@ -1,6 +1,6 @@
 const calc = require('../src/calculator');
 
-test('somma 3 + 5 deve dare 7 (test intenzionalmente sbagliato)', () => {
+test.skip('somma 3 + 5 deve dare 7 (test intenzionalmente sbagliato)', () => {
   const risultato = calc.add(3, 5);
   expect(risultato).toBe(7); // questo fallirà perché 3 + 5 = 8
 });
